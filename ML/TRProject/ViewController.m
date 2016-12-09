96c9efaab1089dbf08648f57ac94b20a1097fee1
Zexi <415338280@qq.com>
2016-12-09 13:49:40 +0800

    工程项目测试

--- ML/TRProject/ViewController.m
+++ ML/TRProject/ViewController.m
@@ -14,6 +14,19 @@
 @implementation ViewController
 - (void)viewDidLoad{
     [super viewDidLoad];
+    [self.view setBackgroundColor:[UIColor whiteColor]];
+    self.navigationItem.title = @"世界你好";
+    
+    UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(100.f, 300.f, 200.f, 40.f)];
+    label.font = [UIFont systemFontOfSize:18.f];
+    label.layer.borderWidth = 2.f;
+    label.layer.borderColor = [UIColor greenColor].CGColor;
+    label.backgroundColor = [UIColor purpleColor];
+    label.textColor = [UIColor whiteColor];
+    label.text = @"hello world!  世界,你好!";
+    
+    [self.view addSubview:label];
+    
 }
 
 - (void)didReceiveMemoryWarning {
